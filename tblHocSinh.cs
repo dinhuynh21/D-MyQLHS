abfdd1fab20abffc9526cc1399a7064d8453b4af
dinhuynh21 <45839628+dinhuynh21@users.noreply.github.com>
2018-12-21 14:56:54 +0700

    Add files via upload
    1-1

---
+++ tblHocSinh.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLHS
+{
+    class tblHocSinh
+    {
+        private string taikhoan;
+        private string matkhau;
+
+        public tblHocSinh(string u,string p)
+        {
+            Taikhoan = u;
+            Matkhau = p;
+        }
+
+        public string Taikhoan { get => taikhoan; set => taikhoan = value; }
+        public string Matkhau { get => matkhau; set => matkhau = value; }
+    }
+}
